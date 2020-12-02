2efb84a6b82da0eff073f35c7b1feac9fd1b69e0
Diogo Rodrigo Schimmelpfennig <diogo_ti@hotmail.com.br>
2020-12-02 17:44:32 -0400

    Projeto

---
+++ TestarSMTP/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace TestarSMTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPorta = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.chkBoxSSL = new System.Windows.Forms.CheckBox();
+            this.txtPara = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(318, 160);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(133, 23);
+            this.btnEnviar.TabIndex = 0;
+            this.btnEnviar.Text = "Enviar e-mail de Teste";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(13, 32);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(249, 20);
+            this.txtHost.TabIndex = 1;
+            this.txtHost.Text = "smtp.gmail.com";
+            // 
+            // txtPorta
+            // 
+            this.txtPorta.Location = new System.Drawing.Point(268, 32);
+            this.txtPorta.Name = "txtPorta";
+            this.txtPorta.Size = new System.Drawing.Size(100, 20);
+            this.txtPorta.TabIndex = 2;
+            this.txtPorta.Text = "587";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(12, 77);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(250, 20);
+            this.txtLogin.TabIndex = 3;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(268, 77);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(186, 20);
+            this.txtSenha.TabIndex = 4;
+            // 
+            // chkBoxSSL
+            // 
+            this.chkBoxSSL.AutoSize = true;
+            this.chkBoxSSL.Checked = true;
+            this.chkBoxSSL.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkBoxSSL.Location = new System.Drawing.Point(374, 35);
+            this.chkBoxSSL.Name = "chkBoxSSL";
+            this.chkBoxSSL.Size = new System.Drawing.Size(46, 17);
+            this.chkBoxSSL.TabIndex = 5;
+            this.chkBoxSSL.Text = "SSL";
+            this.chkBoxSSL.UseVisualStyleBackColor = true;
+            // 
+            // txtPara
+            // 
+            this.txtPara.Location = new System.Drawing.Point(10, 125);
+            this.txtPara.Name = "txtPara";
+            this.txtPara.Size = new System.Drawing.Size(441, 20);
+            this.txtPara.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Host SMTP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Porta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Login";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(265, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Para (E-mail)";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(13, 203);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(441, 176);
+            this.txtLog.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 403);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPara);
+            this.Controls.Add(this.chkBoxSSL);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtPorta);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.btnEnviar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Testar SMTP";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPorta;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.CheckBox chkBoxSSL;
+        private System.Windows.Forms.TextBox txtPara;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ TestarSMTP/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestarSMTP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var email = new MailMessage();
+                email.From = new MailAddress( txtLogin.Text, "Teste de Sistema");
+                email.To.Add(txtPara.Text);
+
+                email.IsBodyHtml = true;
+                email.Subject = "Teste de SMTP";
+                email.Body = "<p>Enviado com sucesso</p>";
+
+                var smtp = new SmtpClient(txtHost.Text, int.Parse( txtPorta.Text));
+                smtp.Credentials = new NetworkCredential(txtLogin.Text, txtSenha.Text);
+                smtp.EnableSsl = chkBoxSSL.Checked;
+
+                smtp.Send(email);
+
+                txtLog.Text = "E-mail enviado com sucesso!!!!";
+
+                MessageBox.Show("E-mail enviado com sucesso!!!");
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text = ex.Message;
+            }
+        } 
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            txtLogin.Focus();
+        }
+    }
+}
